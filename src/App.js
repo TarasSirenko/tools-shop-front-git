965aeb56030bc5afb2edf44a08dd66a68ddbfdd7
Taras Sirenko <tarassirenko71@gmail.com>
2023-08-29 17:08:40 +0300

    Вернул свои изменения

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
 import AppBar from './components/base/AppBar/AppBar';
 import GeneralContainer from './components/base/GeneralContainer/GeneralContainer';
+import { ToastContainer, toast } from 'react-toastify';
 
 import { ModalProvider } from './context/ModalContext';
 
 function App() {
+  const handleClick = e => {
+    console.log(e);
+    toast.error('Вы порвали себе очко!!!!', {
+      position: toast.POSITION.TOP_LEFT,
+      delay: 1000,
+
+      style: {
+        width: '800px',
+        fontSize: '60px', // Здесь вы можете установить нужный размер шрифта
+      },
+    });
+
+    toast.info('Не переживай, заштопаем!', {
+      position: toast.POSITION.TOP_LEFT,
+      delay: 3000,
+
+      style: {
+        width: '800px',
+        fontSize: '60px', // Здесь вы можете установить нужный размер шрифта
+      },
+    });
+
+    const taras = document.querySelector('.taras');
+    taras.textContent = 'Саня, ты в порядке?)';
+  };
   return (
     <ModalProvider>
+      <ToastContainer />
       <GeneralContainer>
         <AppBar />
-        <h1 className="taras">TARAS</h1>
-        <button
-          type="button"
-          onClick={() => {
-            const taras = document.querySelector('.taras');
-            taras.textContent = 'TARAS PETYSHOK';
-          }}
-        >
+        <h1 className="taras">Саня</h1>
+        <button type="button" onClick={handleClick}>
           Нажми меня если не зассал
         </button>
       </GeneralContainer>
--- src/components/forms/LoginForm/LoginForm.js
+++ src/components/forms/LoginForm/LoginForm.js
@@ -34,84 +34,88 @@ export default function LoginForm() {
   };
 
   return (
-    <div className="animate__animated animate__zoomIn animate__faster">
+    <>
       <ToastContainer />
-      <form onSubmit={handleSubmit(onSubmit)} className={s.form}>
-        <div className={s.formTitleWrap}>
-          <h1 className={s.title}>Увійдіть або виконайте</h1>
-          <NavLink to="/register">
-            <button type="button" className={s.registerLink}>
-              Реєстрацію
-            </button>
-          </NavLink>
-        </div>
-        <label className={s.label}>
-          <span className={s.labelTitle}>Пошта</span>
-          <div className={s.inputWrap}>
-            <div className={s.svgWrap}>
-              <IconEmail />
-            </div>
+      <div className="animate__animated animate__zoomIn animate__faster">
+        <form onSubmit={handleSubmit(onSubmit)} className={s.form}>
+          <div className={s.formTitleWrap}>
+            <h1 className={s.title}>Увійдіть або виконайте</h1>
+            <NavLink to="/register">
+              <button type="button" className={s.registerLink}>
+                Реєстрацію
+              </button>
+            </NavLink>
+          </div>
+          <label className={s.label}>
+            <span className={s.labelTitle}>Пошта</span>
+            <div className={s.inputWrap}>
+              <div className={s.svgWrap}>
+                <IconEmail />
+              </div>
 
-            <input
-              {...register('email', {
-                required: 'Введено невірну адресу електронноі пошти',
-                pattern: {
-                  value: /^[\w.-]+@[a-zA-Z_-]+?(?:\.[a-zA-Z]{2,6})+$/,
-                  message: `Електронна пошта повинна містити:\n 
+              <input
+                {...register('email', {
+                  required: 'Введено невірну адресу електронноі пошти',
+                  pattern: {
+                    value: /^[\w.-]+@[a-zA-Z_-]+?(?:\.[a-zA-Z]{2,6})+$/,
+                    message: `Електронна пошта повинна містити:\n 
                     - лише латинські літери;\n
                     - символ "@".`,
-                },
-              })}
-              type="text"
-              name="email"
-              placeholder="Email"
-              autoComplete="email"
-              className={`${s.input} ${errors.email ? s.invalid : s.valid}`}
-            />
-          </div>
-
-          {errors.email && (
-            <FormErrorMessage errorText={errors.email.message} />
-          )}
-        </label>
-        <label className={s.label}>
-          <span className={s.labelTitle}>Пароль</span>
-          <div className={s.inputWrap}>
-            <div className={s.svgWrap}>
-              <IconPassword />
+                  },
+                })}
+                type="text"
+                name="email"
+                placeholder="Email"
+                autoComplete="email"
+                className={`${s.input} ${errors.email ? s.invalid : s.valid}`}
+              />
             </div>
 
-            <input
-              {...register('password', {
-                required: 'Введено невірний пароль',
-                pattern: {
-                  value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{4,16}$/,
-                  message: `Пароль повинен відповідати такому формату:\n
+            {errors.email && (
+              <FormErrorMessage errorText={errors.email.message} />
+            )}
+          </label>
+          <label className={s.label}>
+            <span className={s.labelTitle}>Пароль</span>
+            <div className={s.inputWrap}>
+              <div className={s.svgWrap}>
+                <IconPassword />
+              </div>
+
+              <input
+                {...register('password', {
+                  required: 'Введено невірний пароль',
+                  pattern: {
+                    value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{4,16}$/,
+                    message: `Пароль повинен відповідати такому формату:\n
         - довжина від 4 до 16 символів;\n
         - лише латинські літери та цифри;\n
         - обов'язково має бути хоча б одна велика літера.`,
-                },
-              })}
-              type="password"
-              name="password"
-              placeholder="Password"
-              autoComplete="password"
-              className={`${s.input} ${errors.password ? s.invalid : s.valid}`}
-            />
-          </div>
+                  },
+                })}
+                type="password"
+                name="password"
+                placeholder="Password"
+                autoComplete="password"
+                className={`${s.input} ${
+                  errors.password ? s.invalid : s.valid
+                }`}
+              />
+            </div>
 
-          {errors.password && (
-            <FormErrorMessage errorText={errors.password.message} />
-          )}
-        </label>
+            {errors.password && (
+              <FormErrorMessage errorText={errors.password.message} />
+            )}
+          </label>
 
-        <input
-          className={s.submit}
-          type="submit"
-          value="Вхід"
-          onClick={() => handleClick(errors)}
-        />
-      </form>
-    </div>
+          <input
+            className={s.submit}
+            type="submit"
+            value="Вхід"
+            onClick={() => handleClick(errors)}
+          />
+        </form>
+      </div>
+    </>
   );
 }
